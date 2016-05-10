45ff830a802e8cdf40285b2c4e8bd85d9d931eb0
Anders Olavessen <Anders Olavessen>
2016-05-10 14:50:21 -0500

    adjusted space

--- app/dashboard/parent/route.js
+++ app/dashboard/parent/route.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  session: Ember.inject.service(),
+
   model() {
-    return this.store.findAll(`child`);
-  }
+    const parentId = this.get(`session.data.authenticated.user.id`);
+
+    return this.store.findRecord(`parent`, parentId);
+  },
 });
