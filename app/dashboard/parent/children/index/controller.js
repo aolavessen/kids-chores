be3b927103a789d7c8cba8e3c3e0f49521a1730a
Anders Olavessen <Anders Olavessen>
2016-05-13 17:12:49 -0500

    save child

--- app/dashboard/parent/children/index/controller.js
+++ app/dashboard/parent/children/index/controller.js
@@ -1,11 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  createNewChild(formValues) {
-    const child = this.store.createRecord('child', formValues);
-
-    child.save().then(() => {
-      this.transitionToRoute('dashboard.parent')
-    });
+  delete(child) {
+    if(confirm('Are you sure you want to delete?')) {
+      child.destroyRecord();
+    }
   },
 });
