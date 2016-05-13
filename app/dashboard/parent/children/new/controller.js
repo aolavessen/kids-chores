8d35763c1b0afbc3cc0412542a5c4655abea113d
Anders Olavessen <Anders Olavessen>
2016-05-13 17:26:05 -0500

    more

--- app/dashboard/parent/children/new/controller.js
+++ app/dashboard/parent/children/new/controller.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   saveChild(formValues) {
-    const child = this.store.createRecord(`child`, formValues);
+    const post = this.store.createRecord(`child`, formValues);
 
 
-    child.save().then(() => {
-      this.transitionToRoute(`dashboard.parent`);
+    post.save().then(() => {
+      this.transitionToRoute(`dashboard.parent.children.index`);
     });
   },
 });
