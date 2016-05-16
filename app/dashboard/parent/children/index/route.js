3dba1b418090a36799a24d3e1a5e7683cd3dadfd
Anders Olavessen <Anders Olavessen>
2016-05-16 16:27:43 -0500

    kid works

--- app/dashboard/parent/children/index/route.js
+++ app/dashboard/parent/children/index/route.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  currentUser: Ember.inject.service(),
+
   model() {
-    return this.store.findAll(`child`);
-}
+    return this.store.query(`child`, {
+      filter: {
+        where: {
+          parentId: this.get(`currentUser.id`),
+        },
+      },
+    });
+  },
 });
