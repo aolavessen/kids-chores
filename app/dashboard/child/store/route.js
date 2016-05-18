9db711adffe44ee8d686df10bb2b91621d78e3ab
Joshua Gantt <jr.gantt@icloud.com>
2016-05-18 18:49:44 -0500

    rewards page wip

--- app/dashboard/child/store/route.js
+++ app/dashboard/child/store/route.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model () {
-    return this.modelFor(`dashboard.child`).get(`rewards`);
+    return this.store.findAll(`reward`);
   }
 });
