b6ec556c9c64698e88be71bf62f16ee5042800dc
Anders Olavessen <Anders Olavessen>
2016-05-18 13:25:52 -0500

    child drop down

---
+++ app/dashboard/child/controller.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  delete(assignment) {
+    if (confirm('Are you sure you want to delete?')) {
+      assignment.destroyRecord();
+    }
+  },
+
+  isComplete(assignment) {
+    const disabled = assignment.isComplete;
+    assignment.toggleProperty(`done`);
+    assignment.save();
+  }
+});
